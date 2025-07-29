1503a9f6698f5df33126a97f2c60c865748cfa5b
kwy <365@kangwuyi.com>
2025-07-29 23:22:35 +0800

    update

--- src/components/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    withInstall: <T extends import("vue").Component>(comp: T, name: string) => T & import("vue").Plugin;
-};
-export default _default;
-export * from './install';
--- src/components/install.d.ts
+++
@@ -1,2 +0,0 @@
-import type { Component, Plugin } from 'vue';
-export declare const withInstall: <T extends Component>(comp: T, name: string) => T & Plugin;
